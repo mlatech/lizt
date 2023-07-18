8e6200d189ec6257bda20bb7fe13127723ef085f
Karyna Chernyak <mlatech@icloud.com>
2023-07-18 08:26:59 -0500

    img

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link} from 'react-router-dom';
-import logoImage from "../images/IMG_8098.jpg";
+import logoImage from "./images/IMG_8098.jpg";
 import "./navbar.css"
 
 export default function Navbar(props) {
