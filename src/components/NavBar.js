57768d4d631f15d2f9d880305a3d3610892b724f
Karyna Chernyak <mlatech@icloud.com>
2023-07-17 19:16:26 -0500

    ,

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import logoImage from "/Users/karynachernyak/lizt/src/images/IMG_8098.jpg"; // Replace with the path to your logo image
 import "./navbar.css"
 
