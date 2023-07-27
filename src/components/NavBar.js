cb454e1eb551e95ee4b7e3eb55e1a40001708caf
Karyna Chernyak <mlatech@icloud.com>
2023-07-26 21:40:49 -0500

    added FB  link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logoImage from "./images/IMG_8098.jpg";
-import "./navbar.css"
+import "./navbar.css";
 
 export default function Navbar(props) {
+  const facebookPageURL = "https://www.facebook.com/YourFacebookPage"; // Replace this with your Facebook page URL
 
   return (
     <div className="navbar">
-    <div className="logo">
-    <img src={logoImage} alt="Logo" className="logo-image" />
-      <h3 className='h3'>Tom Peppers General Store</h3>
+      <div className="logo">
+        <img src={logoImage} alt="Logo" className="logo-image" />
+        <h3 className="h3">Tom Peppers General Store</h3>
+      </div>
+      <div className="nav-links">
+        <Link to="/" className="nav-link">Home</Link>
+        {/* <Link to="/plants" className="nav-link">Pre Order</Link> */}
+        <Link to="/info" className="nav-link">Contact</Link>
+      </div>
+      <div className="nav-icons">
+        {/* Facebook Icon Link */}
+        <a href={facebookPageURL} className="nav-icon-link">
+          <i className="fab fa-facebook-square"></i>
+        </a>
+      </div>
     </div>
-    <div className="nav-links">
-      <Link to="/" className="nav-link">Home</Link>
-      {/* <Link to="/plants" className="nav-link">Pre Order</Link> */}
-      <Link to="/info" className="nav-link">Contact</Link>
-      
-    </div>
-    
-  </div>
   );
 }
+
